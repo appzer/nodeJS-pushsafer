e0dc7a0353f72bf2da8e590e9c450eaef7024fc6
Kevin <info@appzer.de>
2016-09-08 16:05:43 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,27 @@
+//var push = require( '../lib/pushsafer.js' );
+var push = require( 'pushsafer-notifications' );
+
+var p = new push( {
+	private_key: '9Ijh32Nj2i9Ole3SXc4D',
+	debug: true,
+	onerror: function(err) {
+		console.log('ERROR:', err);
+	}	
+});
+
+var msg = {
+	m: 'This is a Node.js test message',	// message
+	t: "Node.js Test",						// title
+	s: '8',									// sound (value 0-28)
+	v: '2',									// vibration (empty or value 1-3)
+	i: '5',									// icon (value 1-98)
+	d: '221'								// the device or device group id
+};
+
+// console.log( p );
+
+p.send( msg, function( err, result ) {
+	console.log( 'ERROR:', err );
+	console.log( 'RESULT', result );
+	// process.exit(0);
+});
